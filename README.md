9b247dee57ff0a8434dd5ee190bb20bf3b9d2ee2
tim peterson <trpeters1@hotmail.com>
2013-06-15 02:15:31 -0400

    fixing gh pages

---
+++ README.md
@@ -0,0 +1,38 @@
+Video.js playlist
+===================
+A video-js plugin to play multiple audio tracks or multiple videos.
+
+*Rationale:* The main goal in developing this plugin is to play **audio** files. Playing audio is possible either by swapping the ```<video>``` for an ```<audio>``` tag or by simply feeding audio tracks to the ```<video>``` tag. Both approaches have their pluses and minuses which are [documented here in the video.js issues.](https://github.com/videojs/video.js/issues/537?source=cc)
+
+DEMO
+===================
+[http://tim-peterson.github.io/videojs-playlist/](http://tim-peterson.github.io/videojs-playlist/)
+
+
+Using the Plugin
+----------------
+The plugin automatically registers itself when you include video.playlist.js in your page:
+
+    <script src='videojs.playlist.js'></script>
+
+You probably want to include the default stylesheet, too. It provides some basic styling to indicate what track is currently playing:
+
+    <link href="videojs.playlist.css" rel="stylesheet">
+
+The Playlist plugin currently takes three options, ```tracksClassName```,  ```continuous```, and ```setTrack```:
+
+    myPlayerPlaylist.playlist({
+      'tracksClassName': 'trackSelector', 
+      'continuous': true,
+      'setTrack': 3
+    });
+    
+```**tracksClassName**``` is REQUIRED. It is a string of the className **without** the beginning ```.``` period. The tracksClassName element **requireds** both  ```data-src="/path/to/track.m4a"``` and ```data-index="3"``` attributes in order to switch tracks.  See the HTML in example.html for explanation.
+
+```**continuous**``` is optional. It specifies whether the playlist should play the next track after the previous one finishes. Setting this to ```false``` prevents the continuous playback. Not including this option is the same as setting it to ```true```.
+
+```**setTrack**``` is optional. It allows manually setting the first track that should be played.
+
+Future options being considered include: 1) a shuffle button to shuffle tracks and 2) rendering the playlist using JS rather than as it is currently done with HTML. The intent is to allow a playlist ```{}``` object to be fed in.
+
+
---
+++ javascripts/videojs.playlist.js
@@ -0,0 +1,83 @@
+(function() {
+
+ videojs.plugin('playlist', function(options) {
+
+  /*if(typeof options.render!='undefined' && options.render=='html'){
+     console.log('render html!');
+  }*/
+  console.log('begin playlist plugin');
+   //assign variables
+    var tracks=document.getElementsByClassName(options.tracksClassName),
+        trackCount=tracks.length,
+        player=this,
+        currentTrack=tracks[0],
+        index=0,
+        play=true;
+
+    //manually selecting track
+    for(var i=0; i<trackCount; i++){ 
+     tracks[i].onclick = function(){ 
+        var track=this;
+        index=this.getAttribute('data-index');
+        console.log("a is clicked and index position is"+index+"the data-src is "+this.getAttribute('data-src')); 
+
+        trackSelect(track, player);
+     } 
+    }
+
+    // for continuous play
+    if(typeof options.continuous=='undefined' || options.continuous==true){
+        console.log('play next!');
+
+        player.on("ended", function(){
+            console.log('on ended');   
+             
+            index++;
+            if(index>=trackCount){
+              //console.log('go to beginning');
+              index=0;
+            }
+            else;// console.log('trigger click next track');
+            tracks[index].click();
+          
+        });// on ended     
+    }   
+    else;// console.log('dont play next!');
+
+    //track select function for onended and manual selecting tracks
+    var trackSelect=function(track, player){
+        
+       
+       //get new src
+        var src=track.getAttribute('data-src');
+        console.log('track select click src:'+src);
+        player.src([
+            { type: "audio/mp4", src:  src+".m4a" },
+            { type: "audio/webm", src: src+".webm" },
+            { type: "audio/ogg", src: src+".ogg" }
+          ]); 
+          
+        if(play) player.play();
+
+        //remove 'currentTrack' CSS class 
+        for(var i=0; i<trackCount; i++){ 
+            if(tracks[i].classList.contains('currentTrack')){
+                tracks[i].className=tracks[i].className.replace(/\bcurrentTrack\b/,'');
+            }
+        }
+        //add 'currentTrack' CSS class 
+        track.className = track.className + " currentTrack";
+    }
+
+    //if want to start at track other than 1st track
+    if(typeof options.setTrack!='undefined' ){
+      options.setTrack=parseInt(options.setTrack);
+      currentTrack=tracks[options.setTrack];
+      index=options.setTrack;
+      play=false;
+      console.log('options.setTrack index'+index);
+      trackSelect(tracks[index], player);
+    }
+
+});
+})();
